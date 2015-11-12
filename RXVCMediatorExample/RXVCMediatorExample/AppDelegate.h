4f808f00b2e1b79b0f77932c08aa439b94172c16
Rush.D.Xzj <xzjxylophone@gmail.com>
2015-11-12 17:17:25 +0800

    Add

---
+++ RXVCMediatorExample/RXVCMediatorExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RXVCMediatorExample
+//
+//  Created by Rush.D.Xzj on 15/11/12.
+//  Copyright © 2015年 Rush.D.Xzj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RXVCMediatorExample/RXVCMediatorExample/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  RXVCMediatorExample
+//
+//  Created by Rush.D.Xzj on 15/11/12.
+//  Copyright © 2015年 Rush.D.Xzj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
